ff821c4f2918e25bb5bcef6903e4d9c4df9c86da
Beckett <133655553+severian42@users.noreply.github.com>
2024-03-31 12:55:27 -0500

    Update params.py

--- params.py
+++ params.py
@@ -1,5 +1,4 @@
 OPENAI_MODEL = "gpt-4-turbo-preview"
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
 OUTPUT_FILE_PATH = "./dataset.jsonl"
 NUM_WORKERS = 16
-#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
